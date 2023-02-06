ddae666aeaf1c8a31d1de43f5cacf7fddb4278bf
haerisogyumo <haerisogyumo@gmail.com>
2023-02-06 13:32:27 +0900

    2023-02-06 부산동물병원지도

---
+++ MapBusanAnimalHospital/src/main/java/map/busan/animal/hospital/MapBusanAnimalHospital/controller/MainController.java
@@ -0,0 +1,61 @@
+package map.busan.animal.hospital.MapBusanAnimalHospital.controller;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import map.busan.animal.hospital.MapBusanAnimalHospital.vo.ItemVO;
+import map.busan.animal.hospital.MapBusanAnimalHospital.vo.ResultVO;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+@Controller
+public class MainController {
+    @GetMapping(value={"/", "/index"})
+    public String index(Model model){
+
+        // API 정보
+        String apiURL ="http://apis.data.go.kr/6260000/BusanAnimalHospService/getTblAnimalHospital";
+        String serviceKey ="Gg9AOgBbhCWPW1wtg2mk0CDm70nfTm530rSySwjrZmoZZx/4JBdzcLCrQ580kMG52dyyNn1FEwFFqEkixrllHA==";
+        String resultType ="json";
+        String pageNo ="1";
+        String numOfRows ="1000"; //부산시 전체 동물병원 279
+
+        URI uri = UriComponentsBuilder
+                .fromUriString(apiURL)
+                .queryParam("serviceKey", serviceKey)
+                .queryParam("resultType", resultType)
+                .queryParam("pageNo", pageNo)
+                .queryParam("numOfRows", numOfRows)
+                .encode()
+                .build()
+                .toUri();
+
+        RequestEntity<Void> req = RequestEntity.get(uri).build();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        // JSON 문자열
+        String jsonData = result.getBody();
+
+        // JSON 파싱
+        ObjectMapper om = new ObjectMapper();
+        try{
+            ResultVO resultVO = om.readValue(jsonData, ResultVO.class);
+            ItemVO[] items = resultVO.getGetTblAnimalHospital().getBody().getItems().getItem();
+            model.addAttribute("items", items);
+
+        }catch (JsonMappingException e){
+            e.printStackTrace();
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+        return "index";
+    }
+}
