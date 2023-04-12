9edebe318262630fbbac43caa153ac429699e6e3
haerisogyumo <haerisogyumo@gmail.com>
2023-04-12 13:47:06 +0900

    20230412

--- Ch09/src/main/java/kr/co/ch09/controller/User1Controller.java
+++ Ch09/src/main/java/kr/co/ch09/controller/User1Controller.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.ch09.service.User1Service;
@@ -20,8 +22,9 @@ public class User1Controller {
 	@Autowired
 	private User1Service service;
 	
-	@ResponseBody
-	@GetMapping("/user1")
+	@ResponseBody  
+	/* @CrossOrigin(origins = "*") */
+	@GetMapping("/user1s")
 	public List<User1VO> list() {
 		List<User1VO> users = service.selectUser1s();
 		/*
@@ -36,24 +39,36 @@ public class User1Controller {
 		
 		return users;
 	}
+	//////
+	@ResponseBody
+	/* @CrossOrigin(origins = "*") */
+	@GetMapping("/user1")
+	public User1VO user1(String uid) {
+		return service.selectUser1(uid);
+	}
+	
+	
 	
 	@ResponseBody
+	/* @CrossOrigin(origins = "*") */
 	@GetMapping("/user1/{uid}")
-	public User1VO list(@PathVariable String uid) {
+	public User1VO list(@PathVariable("uid") String uid) {
 		
 		return service.selectUser1(uid);
 	}
 	
 	@ResponseBody
+	/* @CrossOrigin(origins = "*") */
 	@PostMapping("/user1")
-	public List<User1VO> register(User1VO vo) {
+	public List<User1VO> register(@RequestBody User1VO vo) {
 		service.insertUser1(vo);
 		return service.selectUser1s();
 	}
 	
 	@ResponseBody
+	/* @CrossOrigin(origins = "*") */
 	@PutMapping("/user1")
-	public List<User1VO> modify(User1VO vo) {
+	public List<User1VO> modify(@RequestBody User1VO vo) {
 		service.updateUser1(vo);
 		return service.selectUser1s();
 	}
