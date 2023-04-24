76a33c6e9ab2d2edf6e3e631bc735bababcd5cfd
haerisogyumo <haerisogyumo@gmail.com>
2023-04-24 13:48:46 +0900

    20230424

---
+++ TodoApp/src/main/java/kr/co/todo/vo/TodoVO.java
@@ -0,0 +1,14 @@
+package kr.co.todo.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TodoVO {
+
+	private int no;
+	private String content;
+	private int status;
+	private String rdate;
+}
