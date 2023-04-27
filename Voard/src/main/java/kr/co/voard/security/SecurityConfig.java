8c34d91cd6395d73ccbf713a9da0e8a7ce0e3a5a
haerisogyumo <haerisogyumo@gmail.com>
2023-04-27 12:01:38 +0900

    20230427

--- Voard/src/main/java/kr/co/voard/security/SecurityConfig.java
+++ Voard/src/main/java/kr/co/voard/security/SecurityConfig.java
@@ -4,13 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 import kr.co.voard.jwt.JwtAuthenticationFilter;
 
 @Configuration
@@ -21,7 +26,7 @@ public class SecurityConfig {
 	
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
-	
+		
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
